53ac1f6285211800e73280c5d325d360242eaea0
Professor <professor@uixlabs.in>
2023-10-23 15:58:27 +0530

    wip

--- Formula/installer_script.rb
+++ Formula/installer_script.rb
@@ -1,22 +1,22 @@
 class InstallerScript < Formula
   desc "A script to set up my environment"
   homepage "https://github.com/professoruix/installer"
-  url "https://github.com/professoruix/installer/blob/main/installer.sh" # or the direct URL to your script
-  sha256 "faebeb8da80ca8184c42294cf6f02bfdfd9fec510c6cff910ffaa75a9836a2d6" # the SHA-256 of the tarball or script
+  url "https://raw.githubusercontent.com/professoruix/installer/main/installer.sh" # Direct link to the raw script
+  sha256 "faebeb8da80ca8184c42294cf6f02bfdfd9fec510c6cff910ffaa75a9836a2d6"
 
   def install
-    bin.install "installer.sh" => "my-installer"
+    bin.install "installer.sh" => "installer_script"
   end
 
   def caveats
     <<~EOS
       You have installed the installer script.
       To run it, execute the following command:
-          my-installer
+          installer_script
     EOS
   end
 
   test do
-
+    # Ideally, add some test cases here.
   end
 end
